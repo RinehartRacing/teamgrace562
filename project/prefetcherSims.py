26e451b7542c4ef10b21c86cb8d2a401c1f55c30
Gavin Caldwell <70346371+gmcal213@users.noreply.github.com>
2023-04-10 10:53:31 -0700

    only running multi-prefetcher

--- project/prefetcherSims.py
+++ project/prefetcherSims.py
@@ -3,10 +3,9 @@
 import subprocess as sp
 import os
 benches = ["a2time01", "cacheb01", "bitmnp01", "mcf", "libquantum"]
-prefetches = ["StridePrefetcher", "BOPPrefetcher", "TaggedPrefetcher"]
 os.chdir("../gem5")
 for bench in benches:
-    for prefetch in prefetches:
-        command =  f"build/ARM/gem5.opt --stats-file=/home/comparch/Documents/teamgrace562/hw2/benchmarks/{bench}-{prefetch}-stats configs/example/se.py --caches --l1i_size=32kB --l1i_assoc=4 --l1d_size=32kB --l1d_assoc=4 --l1d-hwp-type={prefetch} --cacheline_size=64 --cpu-clock=1.6GHz --cpu-type=O3_ARM_v7a_3 -n 1 --maxinsts=100000000 --bench {bench}"
-        print(command)
-        print(sp.getoutput(command))
+    command =  f"build/ARM/gem5.opt --stats-file=/home/comparch/Documents/teamgrace562/project/benchmarks/{bench}-multiprefetch-stats configs/example/se.py --caches --l1i_size=32kB --l1i_assoc=4 --l1d_size=32kB --l1d_assoc=4 --l1d-hwp-type=MultiPrefetcher --cacheline_size=64 --cpu-clock=1.6GHz --cpu-type=O3_ARM_v7a_3 -n 1 --maxinsts=100000000 --bench {bench}"
+    print(command)
+    print(sp.getoutput(command))
+        
