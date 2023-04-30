c274977007041df86fb5e9ea30f9b513a9b30e8c
Rusty Rinehart <rustyrinehart@email.arizona.edu>
2023-04-30 13:47:26 -0700

    Added graphs

--- project/graphgenerator.py
+++ project/graphgenerator.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
 import argparse
+import sys
 import matplotlib.pyplot as plt
 import pandas as pd
 from glob import glob
 from matplotlib.figure import Figure
-NUM = 50
-def array_thinnerY(big_list):
+def array_thinnerY(big_list, NUM):
+    if NUM == 0:
+        return big_list
     old_length = len(big_list)
     new_length = int(old_length / NUM)
     thin_list = []
+    group_sum = 0
     for i in range(old_length):
-        if i % NUM == 0:
+        if i % NUM == 0 and new_length != 0:
             if i != 0:
                 thin_list.append(group_sum)
             group_sum = 0
@@ -20,7 +23,9 @@ def array_thinnerY(big_list):
         thin_list.append(group_sum)
     return thin_list
 
-def array_thinnerX(big_list):
+def array_thinnerX(big_list, NUM):
+    if NUM == 0:
+        return big_list
     old_length = len(big_list)
     new_length = int(old_length / NUM)
 
@@ -37,7 +42,7 @@ def get_metric(metric_num):
               "Overall Accesses", "Unused Prefetches", "Overall Misses to Overall Accesses Ratio", "Overall Hits to Overall Accesses Ratio",
               "Unused Prefetches to Prefetches Issued Ratio", "Prefetchers in Cache to Unused Prefetches Ratio")
     metrics = ("pfIssued", "pfInCache", "overallMisses", "overallHits", "overallAccesses", "unusedPrefetches",
-               "overallMisses overallAcceses", "overallHits overallAccesses", "unusedPrefetches pfIssued", "pfInCache unusedPrefetches")
+               "overallMisses overallAccesses", "overallHits overallAccesses", "unusedPrefetches pfIssued", "pfInCache unusedPrefetches")
     return (titles[metric_num - 1], metrics[metric_num - 1])
 
 
@@ -61,7 +66,7 @@ def get_markers(index):
     markers = [".", "v", "s", "P", "X", "D", "p", "H"]
     return markers[index]
 
-def graph(title, workload, prefetchers, x_data_set, y_data_set, x_min, x_max):
+def graph(title, workload, prefetchers, x_data_set, y_data_set, x_min, x_max, NUM):
     
     fig = plt.figure(figsize = (5, 4))
 
@@ -74,8 +79,10 @@ def graph(title, workload, prefetchers, x_data_set, y_data_set, x_min, x_max):
         prefetcher = prefetchers[i]
         marker = get_markers(marker_pos % 8)
         marker_pos += 1
-        x_data = array_thinnerX(x_data_set[i])
-        y_data = array_thinnerY(y_data_set[i])
+        
+        x_data = array_thinnerX(x_data_set[i], NUM)
+
+        y_data = array_thinnerY(y_data_set[i], NUM)
         ax1 = ax.plot(x_data, y_data, marker=marker)
     ax.set_xlabel("Instruction Count")
     ax.set_ylabel(title)
@@ -102,7 +109,7 @@ def parse_input(args):
     workload = args.workload
     xmin = args.xmin
     xmax = args.xmax
-
+    NUM = args.NUM
     x_label = "Instruction Count"
     metric = get_metric(metric_num)[1]
     title = get_metric(metric_num)[0]
@@ -111,12 +118,12 @@ def parse_input(args):
     x_data = data_tuple[0]
     y_data = data_tuple[1]
     prefetchers = data_tuple[2]
-    if len(metric) == 2:
-        data_tuple = get_data(metric_split[1]. workload)
+    if len(metric_split) == 2:
+        data_tuple = get_data(metric_split[1], workload)
         y_divisor = data_tuple[1]
-        y_data = list_divider(y_data / y_divisor)
+        y_data = list_divider(y_data, y_divisor)
 
-    graph(title, workload, prefetchers, x_data, y_data, xmin, xmax)
+    graph(title, workload, prefetchers, x_data, y_data, xmin, xmax, NUM)
 
 
 
@@ -131,6 +138,8 @@ def main():
                         type=int, help="Graph minimum x")
     parser.add_argument('-xmax', required=False,
                         type=int,  help="Graph maximum x")
+    parser.add_argument('-NUM', required=False,
+                        type=int,  default=50, help="Thin data by dividing by this number")
     args = parser.parse_args()
     parse_input(args)
 
